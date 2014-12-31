3a5e65b969b995eb27e136d14d1e578bab5f50be
Andrew Millman <andrewmillman35@gmail.com>
2014-12-30 23:24:59 -0500

    Create Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231032036) do
+ActiveRecord::Schema.define(version: 20141231035624) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "brand"
+    t.decimal  "price",       precision: 8,  scale: 2
+    t.decimal  "latitude",    precision: 10, scale: 6
+    t.decimal  "longitude",   precision: 10, scale: 6
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
