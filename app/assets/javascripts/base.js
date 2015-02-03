0131a7235d57a239fb65d6acd511c43aaf7ea8d4
Andrew Millman <andrewmillman35@gmail.com>
2015-02-02 23:22:19 -0800

    Add proper animation for the navbar matieralizing / dematerializing during scroll

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -11,6 +11,15 @@ $(document).on( 'page:change', function() {
 
   if ($('.static-pages-home').length) {
     $('#menu, .container, .cadeau-navbar').addClass('home');
+
+    $(window).scroll( function() {
+      console.log('sd');
+      if ($(this).scrollTop() > 500 - 64) {
+        $('.cadeau-navbar').removeClass('home');
+      } else {
+        $('.cadeau-navbar').addClass('home');
+      }
+    });
   }
 
   $('.products-index').masonry({
