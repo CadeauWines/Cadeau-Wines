f4ca2e93622c5802a4a29fb44de68686b6bc084f
Andrew Millman <andrewmillman35@gmail.com>
2015-02-09 23:54:37 -0800

    Comment out unreliable fade transitions between page switches

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,9 +1,9 @@
 $(document).on( 'page:fetch', function() {
-  $('#content').removeClass('show');
+  //$('#content').removeClass('show');
 });
 
 $(document).on( 'page:change', function() {
-  $('#content').addClass('show');
+  //$('#content').addClass('show');
 
   $('#menu-button').on('click', function() {
     $('#menu').toggleClass('open');
